6b6486f57492b7b7fbfeaeacaf7c5e234c7ed1c5
idmyn <hello@davidmyno.rs>
2019-09-20 08:02:15 +0100

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    save
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    save
+  end
 end
